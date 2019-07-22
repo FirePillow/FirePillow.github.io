82d985b6ba08100dae90c2985d52612a382c4dc7
Mike Lu <heatmint2333@gmail.com>
2019-07-23 01:49:04 +0800

    General formatting

--- forum/vue/openpage/app.js
+++ forum/vue/openpage/app.js
@@ -1,5 +1,4 @@
-datalist = [
-    {
+datalist = [{
         avatarurl: "../common/sampleavatar.png",
         badge1: "DP",
         color1: "bgred",
@@ -7,7 +6,7 @@ datalist = [
         color2: "bggreen",
         title: "Battle of Bands 开始报名",
         stat: "Abby 回复于 13:13",
-        postcount:3,
+        postcount: 3,
         body: "SDNSADNJSDNAKJSNKJANSDKAJNSDKNASDJNAKCNJHUGEBGWBUIDFNJDNCSNFFINDSDFVAbbyNFJSDNFSDCNCIUENWINFIDSNFDISNSDINUCSIUENFIEUBRFISDNFOSNFONEIBFSDNFJSDNFKSDJNFJKNKSDJSDNSADNJSDNAKJSNKJANSDKAJNSDKNASDJNAKCNJHUGEBGWBUIDFNJDNCSNFFINDSDFVAbbyNFJSDNFSDCNCIUENWINFIDSNFDISNSDINUCSIUENFIEUBRFISDNFOSNFONEIBFSDNFJSDNFKSDJNFJKNKSDJN"
     },
     {
@@ -18,7 +17,7 @@ datalist = [
         color2: "bggreen",
         title: "Battle of Bands 开始报名",
         stat: "Abby 回复于 13:13",
-        postcount:3,
+        postcount: 3,
         body: "SDNSADNJSDNAKJSNKJANSDKAJNSDKNASDJNAKCNJHUGEBGWBUIDFNJDNCSNFFINDSDFVAbbyNFJSDNFSDCNCIUENWINFIDSNFDISNSDINUCSIUENFIEUBRFISDNFOSNFONEIBFSDNFJSDNFKSDJNFJKNKSDJSDNSADNJSDNAKJSNKJANSDKAJNSDKNASDJNAKCNJHUGEBGWBUIDFNJDNCSNFFINDSDFVAbbyNFJSDNFSDCNCIUENWINFIDSNFDISNSDINUCSIUENFIEUBRFISDNFOSNFONEIBFSDNFJSDNFKSDJNFJKNKSDJN"
     }
 ];
@@ -26,26 +25,26 @@ datalist = [
 Vue.component('preposts', {
     props: ['data'],
     template: `
-			<div class="prepost">
-				<img class="prepostimg" v-bind:src="data.avatarurl">
-				<div class="prepcontent">
-					<div class="badge">
-						<span class="badge1" v-bind:class="data.color1">DP</span><span class="badge2" v-bind:class="data.color2">活动</span>
-					</div>
-					<div class="prepcomments">
-						<span><i class="fa fa-comments" aria-hidden="true"></i> {{data.postcount}}</span>
-					</div>
-					<div class="preptitle">
-						<b>{{data.title}}</b>
-					</div>
-					<div class="prepstat">
-						<span>{{data.stat}}</span>
-					</div>
-					<div class="prepbody">
-						<span>{{data.body}}</span>
-					</div>
-				</div>
-			</div>
+            <div class="prepost">
+                <img class="prepostimg" v-bind:src="data.avatarurl">
+                <div class="prepcontent">
+                    <div class="badge">
+                        <span class="badge1" v-bind:class="data.color1">DP</span><span class="badge2" v-bind:class="data.color2">活动</span>
+                    </div>
+                    <div class="prepcomments">
+                        <span><i class="fa fa-comments" aria-hidden="true"></i> {{data.postcount}}</span>
+                    </div>
+                    <div class="preptitle">
+                        <b>{{data.title}}</b>
+                    </div>
+                    <div class="prepstat">
+                        <span>{{data.stat}}</span>
+                    </div>
+                    <div class="prepbody">
+                        <span>{{data.body}}</span>
+                    </div>
+                </div>
+            </div>
   `
 });
 
